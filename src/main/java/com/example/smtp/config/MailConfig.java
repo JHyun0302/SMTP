504a0127f96ef5e52d6aa3a31129531fa099c80a
JHyun <wogustkfkd11@naver.com>
2024-12-06 14:14:26 +0900

    add google SMTP

--- src/main/java/com/example/smtp/config/MailConfig.java
+++ src/main/java/com/example/smtp/config/MailConfig.java
@@ -1,10 +1,8 @@
 package com.example.smtp.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -16,7 +14,8 @@ public class MailConfig {
 
     private final MailClient client;
     @Bean
-    public JavaMailSender NaverMailService() {
+    public JavaMailSender naverMailService() {
+        boolean isSSL = true;
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost(client.getHost());
@@ -25,19 +24,37 @@ public class MailConfig {
 
         javaMailSender.setPort(client.getPort()); // SMTP port
 
-        javaMailSender.setJavaMailProperties(getMailProperties()); //메일 인증서버 가져오기
+        javaMailSender.setJavaMailProperties(getMailProperties(isSSL)); //메일 인증서버 가져오기
 
         return javaMailSender;
     }
 
-    private Properties getMailProperties() {
+    @Bean
+    public JavaMailSender googleMailService() {
+        boolean isSSL = false;
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost(client.getHost());
+        javaMailSender.setUsername(client.getUsername()); //네이버 아이디
+        javaMailSender.setPassword(client.getPassword()); //네이버 비밀번호
+
+        javaMailSender.setPort(client.getPort()); // SMTP port
+
+        javaMailSender.setJavaMailProperties(getMailProperties(isSSL)); //메일 인증서버 가져오기
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties(boolean isSSL) {
         Properties properties = new Properties();
         properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
         properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
         properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
         properties.setProperty("mail.debug", "true"); // 디버그 사용
-        properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com"); // ssl 인증 서버 (smtp 서버명)
-        properties.setProperty("mail.smtp.ssl.enable", "true"); // ssl 사용
+        if (isSSL) {
+            properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com"); // ssl 인증 서버 (smtp 서버명) - google은 ssl 지원 x
+            properties.setProperty("mail.smtp.ssl.enable", "true"); // ssl 사용
+        }
 
         return properties;
     }
